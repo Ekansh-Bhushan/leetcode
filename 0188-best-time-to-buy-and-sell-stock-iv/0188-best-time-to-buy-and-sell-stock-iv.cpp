aabd9520ea9a9fbaffb0ed0893e9655dea2de9fa
Ekansh Bhushan <129383527+Ekansh-Bhushan@users.noreply.github.com>
2025-08-28 00:59:44 +0530

    Time: 32 ms (14.67%), Space: 14.3 MB (88.82%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -5,18 +5,20 @@ public:
         n = prices.size();
         if (n == 0 || k == 0) return 0;
 
-        vector<vector<int>> dp(n+1, vector<int>(2*k+1, 0));
+        vector<int> ahead(2*k+1, 0);
+        vector<int> curr(2*k+1, 0);
 
         for (int i = n-1; i >= 0; i--) {
             for (int transaction = 0; transaction < 2*k; transaction++) {
                 if (transaction % 2 == 0) {
-                    dp[i][transaction] = max(-prices[i] + dp[i+1][transaction+1],dp[i+1][transaction]);
+                    curr[transaction] = max(-prices[i] + ahead[transaction+1],ahead[transaction]);
                 } else {
-                    dp[i][transaction] = max(prices[i] + dp[i+1][transaction+1],dp[i+1][transaction]);
+                    curr[transaction] = max(prices[i] + ahead[transaction+1],ahead[transaction]);
                 }
+                ahead = curr;
             }
         }
 
-        return dp[0][0];
+        return ahead[0];
     }
 };
