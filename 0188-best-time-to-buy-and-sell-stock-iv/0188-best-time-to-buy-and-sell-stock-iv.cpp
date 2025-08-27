61d0b39cf2f4f7332ad3ba04e3426448ab889a0e
Ekansh Bhushan <129383527+Ekansh-Bhushan@users.noreply.github.com>
2025-08-28 00:45:33 +0530

    Time: 11 ms (46.65%), Space: 17.8 MB (58.75%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,26 +1,28 @@
 class Solution {
     int n;
+
+    int solve(int index, int transaction,int k,vector<int>& prices,vector<vector<int>>& dp) {
+        if(index == n || transaction == 2*k ) return 0;
+
+
+        if(dp[index][transaction] != -1) return dp[index][transaction];
+
+        int profit = 0;
+
+
+        if(transaction%2 == 0) {
+            profit = max(-prices[index] + solve(index+1 , transaction + 1, k , prices,dp),solve(index+1 , transaction, k , prices,dp) );
+        } else {
+            profit = max(prices[index] + solve(index+1 , transaction + 1, k , prices,dp),solve(index+1 , transaction, k , prices,dp) );
+        }
+
+        return dp[index][transaction] = profit;
+    }
 public:
     int maxProfit(int k, vector<int>& prices) {
         n = prices.size();
-
-
-        vector<vector<int>> ahead(2, vector<int> (k+1,0));
-        vector<vector<int>> curr(2, vector<int> (k+1,0));
-
-        for(int i = n-1 ; i>=0 ; i--) {
-            for(int buy = 0; buy < 2 ; buy++) {
-                for(int trancation = 0 ; trancation < k ; trancation++) {
-                    if(buy == 1) {
-                        curr[buy][trancation] = max(-prices[i] + ahead[0][trancation], ahead[1][trancation]);
-                    } else {
-                        curr[buy][trancation] = max(prices[i] + ahead[1][trancation+1], ahead[0][trancation]);
-                    }
-                    ahead =curr;
-                }
-            }
-        }
-
-        return ahead[1][0];
+        vector<vector<int>> dp(n+1,vector<int> (2*k+1,-1));
+        return solve(0,0,k,prices,dp);
+       
     }
 };
