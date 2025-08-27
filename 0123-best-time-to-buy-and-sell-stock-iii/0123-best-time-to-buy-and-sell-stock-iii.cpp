a26a6234f1a9928065ef9053e27b3a14ef2df524
Ekansh Bhushan <129383527+Ekansh-Bhushan@users.noreply.github.com>
2025-08-27 23:30:03 +0530

    Time: 158 ms (61.72%), Space: 139.6 MB (60.77%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,23 +1,19 @@
 class Solution {
     int n;
-    int solve(int index, int tranction, vector<int>& prices,vector<vector<int>>& dp) {
-        if(index == n || tranction == 4) return 0;
-        if(dp[index][tranction] != -1) return dp[index][tranction];
-        int profit = 0;
-        if(tranction % 2 == 0) {
-            profit = max(-prices[index] + solve(index+1,tranction+1,prices,dp), solve(index+1,tranction,prices,dp));
-        } else {
-            profit = max(prices[index] + solve(index+1,tranction+1,prices,dp), solve(index+1,tranction,prices,dp));
-        }
-
-        return dp[index][tranction] = profit;
-    }
 public:
     int maxProfit(vector<int>& prices) {
-        n = prices.size();
+        int n = prices.size();
+        vector<vector<int>> dp(n+1, vector<int>(5, 0));
 
-        vector<vector<int>> dp(n+1, vector<int> (4,-1));
-
-        return solve(0,0,prices,dp);
+        for(int i = n-1; i >= 0; i--) {
+            for(int tranction = 0; tranction < 4; tranction++) {
+                if(tranction % 2 == 0) { 
+                    dp[i][tranction] = max(-prices[i] + dp[i+1][tranction+1], dp[i+1][tranction]);
+                } else { 
+                    dp[i][tranction] = max(prices[i] + dp[i+1][tranction+1], dp[i+1][tranction]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
