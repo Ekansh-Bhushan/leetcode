eb669ed3760b6f350841da7604b56bdda17d171e
Ekansh Bhushan <129383527+Ekansh-Bhushan@users.noreply.github.com>
2025-08-30 20:29:54 +0530

    Time: 427 ms (14.79%), Space: 390.3 MB (5.27%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,13 +1,12 @@
 class Solution {
     string checkPalindrom;
-    bool isPalindrom(int l, int r) {
-        int n = checkPalindrom.length();
-        while(l < r) {
-            if(checkPalindrom[l] != checkPalindrom[r]) return false;
-            l++;
-            r--;
-        } 
-        return true;
+    bool isPalindrom(int l, int r,vector<vector<int>>& dp) {
+        if(l >= r) return dp[l][r] = 1;
+
+        if(dp[l][r] != -1) return dp[l][r];
+
+        if(checkPalindrom[l] == checkPalindrom[r]) return dp[l][r] = isPalindrom(l+1,r-1,dp);
+        return dp[l][r] =  0;
     }
 public:
     string longestPalindrome(string s) {
@@ -15,9 +14,12 @@ public:
         int maxLen = 0;
         int startingPoint = -1;
         checkPalindrom = s;
+        
+        vector<vector<int>> dp(1001, vector<int> (1001,-1));
+
         for(int i = 0 ; i < n ; i++) {
             for(int j = i ; j < n ; j++) {
-                if(isPalindrom(i,j)) {
+                if(isPalindrom(i,j,dp)) {
                     if(maxLen < j - i + 1) {
                         maxLen = j - i + 1;
                         startingPoint = i;
