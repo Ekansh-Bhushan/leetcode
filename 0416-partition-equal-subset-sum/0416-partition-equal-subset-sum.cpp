4f46eae8c2c00f39381d66f1e9b776d6a4d570db
Ekansh Bhushan <129383527+Ekansh-Bhushan@users.noreply.github.com>
2025-09-04 15:16:11 +0530

    Time: 125 ms (63.44%), Space: 13.5 MB (83.83%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -12,29 +12,32 @@ public:
 
         if(totalSum % 2 != 0) return false;
 
-        vector<vector<bool>> dp(n+1,vector<bool> ((totalSum/2)+1,false));
         int k = totalSum/2;
+        vector<bool> prev(k+1,false);
+        vector<bool> curr(k+1,false);
         
-        for(int i= 0 ;i < n ;i++) {
-            dp[i][0]=true;
-        }
+        
+        prev[0]=true;
+        curr[0] = true;
+        
 
-        if(nums[0] <= k) dp[0][nums[0]] = true;
+        if(nums[0] <= k) prev[nums[0]] = true;
 
         for(int i= 1 ;i < n ; i++){
             for(int target = 0; target <= k ;target++) {
-                bool notPick = dp[i-1][target];
+                bool notPick = prev[target];
 
                 bool pick = false;
 
                 if(target >= nums[i]){
-                    pick = dp[i-1][target - nums[i]];
+                    pick = prev[target - nums[i]];
                 }
 
-                dp[i][target] = pick | notPick;
+                curr[target] = pick | notPick;
             }
+            prev = curr;
         }
 
-        return dp[n-1][k];
+        return prev[k];
     }
 };
