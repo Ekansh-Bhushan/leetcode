0e7140f635f2d0d8e02d0f42d89109e4812aab2f
Ekansh Bhushan <129383527+Ekansh-Bhushan@users.noreply.github.com>
2025-09-04 15:14:43 +0530

    Time: 147 ms (39.83%), Space: 17.5 MB (55.54%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,24 +1,6 @@
 class Solution {
     int n;
 
-    bool solve(int index, int target, vector<int>& nums,vector<vector<int>>& dp){
-        if(target == 0) return true;
-        if(index == 0) {
-            if(target == nums[index]) return true;
-            return false;
-        }
-        if(dp[index][target] != -1) return dp[index][target];
-
-        bool notPick = solve(index-1 , target , nums,dp);
-
-        bool pick = false;
-
-        if(target >= nums[index]){
-            pick = solve(index-1, target - nums[index] , nums,dp);
-        }
-
-        return dp[index][target] = pick | notPick;
-    }
 public:
     bool canPartition(vector<int>& nums) {
         n = nums.size();
@@ -30,8 +12,29 @@ public:
 
         if(totalSum % 2 != 0) return false;
 
-        vector<vector<int>> dp(n+1,vector<int> ((totalSum/2)+1,-1));
+        vector<vector<bool>> dp(n+1,vector<bool> ((totalSum/2)+1,false));
+        int k = totalSum/2;
+        
+        for(int i= 0 ;i < n ;i++) {
+            dp[i][0]=true;
+        }
 
-        return solve(n-1,totalSum/2,nums,dp);
+        if(nums[0] <= k) dp[0][nums[0]] = true;
+
+        for(int i= 1 ;i < n ; i++){
+            for(int target = 0; target <= k ;target++) {
+                bool notPick = dp[i-1][target];
+
+                bool pick = false;
+
+                if(target >= nums[i]){
+                    pick = dp[i-1][target - nums[i]];
+                }
+
+                dp[i][target] = pick | notPick;
+            }
+        }
+
+        return dp[n-1][k];
     }
 };
