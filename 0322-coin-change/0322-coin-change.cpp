68a71b4614a09f8bad02185bc30739a5cab92f77
Ekansh Bhushan <129383527+Ekansh-Bhushan@users.noreply.github.com>
2025-09-06 00:02:03 +0530

    Time: 50 ms (27.6%), Space: 48.7 MB (47.72%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,29 +1,28 @@
 class Solution {
-    int n ;
-    int solve(int index, int amount, vector<int>& coins,vector<vector<int>>& dp) {
-        if(amount == 0) return 0;
-        if(index ==0) {
-            if(amount%coins[0] == 0) return amount/coins[0];
-            return 1e9;
-        }
-
-        if(dp[index][amount] != -1) return dp[index][amount];
-
-        int notPick = solve(index-1 , amount, coins,dp);
-        int pick = INT_MAX;
-        if(amount >= coins[index]) {
-            pick = 1 + solve(index,amount - coins[index],coins,dp);
-        }
-
-        return dp[index][amount] = min(pick,notPick);
-    }
 public:
     int coinChange(vector<int>& coins, int amount) {
-        if(amount == 0) return 0;
-        n= coins.size();
+        if (amount == 0) return 0;
+        int n = coins.size();
 
-        vector<vector<int>> dp(n+1 , vector<int> (amount + 1, -1));
-        int ans = solve(n-1,amount,coins,dp);
-        return ans == 1e9 ? -1 : ans;
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 1e9));
+
+        for (int target = 0; target <= amount; target++) {
+            if (target % coins[0] == 0)
+                dp[0][target] = target / coins[0];
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int target = 0; target <= amount; target++) {
+                int notPick = dp[i - 1][target];
+                int pick = 1e9;
+                if (target >= coins[i]) {
+                    pick = 1 + dp[i][target - coins[i]];
+                }
+                dp[i][target] = min(pick, notPick);
+            }
+        }
+
+        int ans = dp[n - 1][amount];
+        return ans >= 1e9 ? -1 : ans;
     }
 };
