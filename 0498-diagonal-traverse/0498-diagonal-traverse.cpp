fa4156b6ad97a30d552c6a82315a13337905d62a
Ekansh Bhushan <129383527+Ekansh-Bhushan@users.noreply.github.com>
2025-09-06 11:00:09 +0530

    Time: 27 ms (18.43%), Space: 31.2 MB (11.62%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
-        if (matrix.empty() || matrix[0].empty()) return {};
+        if(matrix.empty()) return {};
 
-        int m = matrix.size(), n = matrix[0].size();
-        vector<int> result(m * n);
-        int row = 0, col = 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        map<int, vector<int>> mpp;
 
-        for (int i = 0; i < m * n; i++) {
-            result[i] = matrix[row][col];
-
-            if ((row + col) % 2 == 0) {
-                if (col == n - 1) row++;
-                else if (row == 0) col++;
-                else { row--; col++; }
-            } else {
-                if (row == m - 1) col++;
-                else if (col == 0) row++;
-                else { row++; col--; }
+        // Group elements by diagonal key (i+j)
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                mpp[i + j].push_back(matrix[i][j]);
             }
         }
 
-        return result;
+        vector<int> res;
+
+        for(auto &it : mpp) {
+            if(it.first % 2 == 0) {
+                // Reverse for even diagonals
+                reverse(it.second.begin(), it.second.end());
+            }
+            // Insert all elements into result
+            for(int val : it.second) {
+                res.push_back(val);
+            }
+        }
+
+        return res;
     }
 };
