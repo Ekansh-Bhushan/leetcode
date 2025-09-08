6bef5ca2b318f14b77bb95912917055d3275dee1
Ekansh Bhushan <129383527+Ekansh-Bhushan@users.noreply.github.com>
2025-09-08 14:51:11 +0530

    Time: 70 ms (6.92%), Space: 31.6 MB (38.49%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -2,11 +2,11 @@ class Solution {
     int n;
 
     int solve(int i ,int j , string &s, string &t,vector<vector<int>>& dp) {
-        if(i < 0 || j < 0 ) return 0;
+        if(i == 0 || j == 0 ) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        if(s[i] == t[j]) {
+        if(s[i-1] == t[j-1]) {
             return dp[i][j] = 1 + solve(i-1,j-1,s,t,dp);
         }
         return dp[i][j] = max(solve(i-1,j,s,t,dp),solve(i,j-1,s,t,dp));
@@ -15,7 +15,7 @@ class Solution {
     int longestPalindromicSubsequence(string &s, string &t) {
         vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
 
-        return solve(n-1,n-1,s,t,dp);
+        return solve(n,n,s,t,dp);
     }
 public:
     int minInsertions(string s) {
