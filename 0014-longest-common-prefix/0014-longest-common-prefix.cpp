ccd803a06ecf0d26dece36027de30a35e730ca43
Ekansh Bhushan <129383527+Ekansh-Bhushan@users.noreply.github.com>
2025-10-04 00:30:13 +0530

    Time: 0 ms (100%), Space: 12.6 MB (7.29%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,24 +1,85 @@
+class Node{
+    public:
+        Node* links[26];
+        int cntChildren = 0;
+        bool end = false;
+
+        bool containsKey(char ch){
+            return links[ch-'a'] != NULL;
+        }
+
+        void put(char ch, Node* node){
+            links[ch-'a'] = node;
+        }
+
+        Node* get(char ch){
+            return links[ch-'a'];
+        }
+
+        void incChildren(){
+            cntChildren++;
+        }
+
+        void setEnd(){
+            end = true;
+        }
+
+        bool isEnd(){
+            return end;
+        }
+};
+
+class Trie{
+    Node* root;
+
+    public: 
+        Trie(){
+            root = new Node();
+        }
+
+        void insert(string& word){
+            Node* node = root;
+            for(int i = 0 ; i < word.length() ; i++) {
+                if(!node->containsKey(word[i])) {
+                    node->put(word[i],new Node());
+                    node->incChildren();
+                }
+                node = node->get(word[i]);
+            }
+            node->setEnd();
+        }
+
+
+        string preficLCP(string& word) {
+            Node* node = root;
+            string ans = "";
+
+            for(int i = 0 ; i < word.length() ; i++) {
+                char ch = word[i];
+
+                if(node->cntChildren == 1 && !node->isEnd()){
+                    ans += ch;
+                } else {
+                    break;
+                }
+                node = node->get(ch);
+            }
+            return ans;
+        }
+
+};
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int n = strs.size();
+        Trie trie;
 
-        sort(strs.begin(), strs.end());
-
-        string first = strs[0];
-        string last = strs[n-1];
-
-        int minlen = min(first.length(),last.length());
-
-        string ans = "";
-
-        for(int i = 0 ; i < minlen ; i++) {
-            if(first[i] != last[i]) {
-                break;
-            }
-            ans += first[i];
+        for(auto str: strs){
+            trie.insert(str);
         }
 
-        return ans;
+        string first = strs[0];
+        
+        return trie.preficLCP(first);
     }
 };
