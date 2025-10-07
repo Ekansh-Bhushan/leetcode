a224b09a73d92a4b6291e8014f473c0b9ae61b97
Ekansh Bhushan <129383527+Ekansh-Bhushan@users.noreply.github.com>
2025-10-07 10:01:34 +0530

    Time: 0 ms (100%), Space: 7.7 MB (87.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,25 +1,13 @@
 class Solution {
-    int solve(int n , vector<int>& dp){
-        if(n == 0) return 1;
-        if(n == 1) return 1;
-
-        if(dp[n] != -1) return dp[n];
-
-        int left = climbStairs(n-1);
-        int right = climbStairs(n-2);
-        return dp[n] = left + right;
-    }
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1,0);
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for(int i = 2; i<= n ;i++) {
-            int left = dp[i-1];
-            int right = dp[i-2];
-            dp[i] = left + right;
+        int prev2 = 1;
+        int prev = 1;
+        for(int i = 2; i<= n;i++) {
+            int curr = prev + prev2;
+            prev2= prev;
+            prev = curr;
         }
-        return dp[n];
+        return prev;
     }
 };
