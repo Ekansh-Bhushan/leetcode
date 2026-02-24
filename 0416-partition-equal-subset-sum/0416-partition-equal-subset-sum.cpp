43c14dda64dfbbd50005c09e0580f6a242ed46b5
Ekansh Bhushan <129383527+Ekansh-Bhushan@users.noreply.github.com>
2026-02-24 15:56:52 +0530

    Time: 155 ms (31.78%), Space: 17.6 MB (54.1%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -22,10 +22,29 @@ public:
 
         if(totalSum%2 == 1) return false;
 
-        int target = totalSum/2;
+        int k = totalSum/2;
 
-        vector<vector<int>> dp(n+1, vector<int> (target + 1,-1));
+        vector<vector<bool>> dp(n+1, vector<bool> (k + 1,false));
 
-        return subsetSum(n-1,target,nums,dp);
+
+        for(int i = 0; i < n ; i++) {
+            dp[i][0] = true;
+        }
+
+        if(nums[0] <= k)
+            dp[0][nums[0]] = true;
+
+        for(int index =1; index < n ; index++) {
+            for(int target = 1 ; target <= k; target++) {
+                bool notTaken = dp[index - 1][target];
+                bool Taken = false;
+                if(nums[index] <= target) {
+                    Taken = dp[index - 1][target - nums[index]];
+                }
+                dp[index][target]  = Taken || notTaken;
+            }
+        }
+
+        return dp[n-1][k];
     }
 };
