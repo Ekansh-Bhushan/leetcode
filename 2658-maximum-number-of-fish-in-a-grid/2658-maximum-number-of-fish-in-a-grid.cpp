db3b2cc1f6f833d223d53b705a8ba66be1811c91
Ekansh Bhushan <129383527+Ekansh-Bhushan@users.noreply.github.com>
2026-03-16 14:39:31 +0530

    Time: 15 ms (39.86%), Space: 107.9 MB (31.09%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -17,6 +17,37 @@ class Solution {
         }
         return sum;
     }
+
+    int bfs(int row, int col, vector<vector<int>>& grid) {
+        int sum = grid[row][col];
+        
+
+        queue<pair<int,int>> q;
+        q.push({row,col});
+        grid[row][col] = 0;
+
+        int delRow[] = {-1,0,1,0};
+        int delCol[] = {0,-1,0,1};
+
+        while(!q.empty()) {
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+
+            for(int k = 0; k < 4 ; k++) {
+                int newRow = i + delRow[k];
+                int newCol = j + delCol[k];
+
+                if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && grid[newRow][newCol] != 0){
+                    sum += grid[newRow][newCol];
+                    grid[newRow][newCol] = 0;
+                    q.push({newRow,newCol});
+                }
+            }
+        }
+
+        return sum;
+    }
 public:
     int findMaxFish(vector<vector<int>>& grid) {
         n = grid.size();
@@ -27,7 +58,7 @@ public:
         for(int i = 0; i < n ; i++) {
             for(int j = 0; j < m; j++) {
                 if(grid[i][j] != 0) {
-                    int fish = dfs(i,j,grid);
+                    int fish = bfs(i,j,grid);
                     maxFish = max(maxFish,fish);
                 }
             }
